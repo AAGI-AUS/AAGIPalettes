1f14b9e0fc21dba78b42a7f8fe8ba0e8ca7fe490
Adam H. Sparks <adamhsparks@icloud.com>
2024-07-16 18:47:05 +0800

    Add grey from table body to colours

--- R/data.R
+++ R/data.R
@@ -4,15 +4,16 @@
 #'   provided via [aagi_palettes()] for plotting purposes but may not considered
 #'   to be "official" \acronym{AAGI} colours.
 #'
-#' @format A `vector` with seven (7) colours as hexadecimal values:
+#' @format A `vector` with eight (8) colours as hexadecimal values:
 #'   \describe{
-#'      \item{AAGI Black}{#414042}
 #'      \item{AAGI Bright Green}{#B6D438}
 #'      \item{AAGI Teal}{#00808B}
 #'      \item{AAGI Blue}{#648FD2}
 #'      \item{AAGI Green}{#54921E}
 #'      \item{AAGI Yellow}{#FFBC42}
 #'      \item{AAGI Orange}{#ec8525}
+#'      \item{AAGI Black}{#414042}
+#'      \item{AAGI Grey}{#F2F2F2}
 #'      }
 #' 
 #' @keywords datasets
